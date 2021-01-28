b25111e992c913c414ed2b6df158fe75e5a18275
1nVitr0 <becker.aram@gmail.com>
2021-01-29 00:18:56 +0100

    feat: add bad time completion

--- shared/src/tools/time.ts
+++ shared/src/tools/time.ts
@@ -1,5 +1,6 @@
 import moment, { Moment, Duration } from 'moment';
 import { defaultBasicSettings } from '../model/Defaults/DefaultSummarySettings';
+import DefaultTime from '../model/Defaults/DefaultTime';
 import { formatString } from './string';
 
 export function validateDate(date: string, settings = defaultBasicSettings): boolean {
@@ -17,6 +18,14 @@ export function formatDate(date: Moment, settings = defaultBasicSettings): strin
 
 export function parseDuration(minutes: number, settings = defaultBasicSettings): Duration {
   return moment.duration(minutes, 'm');
+}
+
+export function parseTime(time: string, settings = defaultBasicSettings): Moment {
+  return moment(DefaultTime.extendTime(time), DefaultTime.extendTimeFormat(settings.timeFormat));
+}
+
+export function formatTime(time: Moment, settings = defaultBasicSettings): string {
+  return time.format(settings.timeFormat);
 }
 
 export function formatDuration(_duration: Duration | number, settings = defaultBasicSettings) {
