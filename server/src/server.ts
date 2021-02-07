34cac767925536a65680dd5bd2c61ec6d6401efc
1nVitr0 <becker.aram@gmail.com>
2021-02-07 15:21:15 +0100

    fix: include newline as trigger character

--- server/src/server.ts
+++ server/src/server.ts
@@ -52,7 +52,7 @@ connection.onInitialize((params: InitializeParams) => {
       // Tell the client that this server supports code completion.
       completionProvider: {
         resolveProvider: true,
-        triggerCharacters: ["'", '"', ' '],
+        triggerCharacters: ["'", '"', ' ', '\n'],
       },
     },
   };
