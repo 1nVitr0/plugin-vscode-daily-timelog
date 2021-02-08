21d1a3359274ece7cc21b54fa24f2481f6239578
1nVitr0 <becker.aram@gmail.com>
2021-02-08 22:09:35 +0100

    feat: add option for custom params

--- shared/src/model/Summary/Params.ts
+++ shared/src/model/Summary/Params.ts
@@ -1,6 +1,15 @@
 import { Settings } from 'http2';
 import { SummarySettings, TaskListSettings } from './Settings';
 
+export enum ParamType {
+  Array = 'array',
+  String = 'string',
+  Date = 'date',
+  Time = 'time',
+  Duration = 'duration',
+  Task = 'task',
+}
+
 export interface BasicParams extends Settings {
   date: string;
   durations: string;
@@ -24,4 +33,12 @@ export interface DurationListParams extends TaskListParams, SummaryParams {
 export interface OverviewParams extends DurationListParams {
   summary: string;
   taskList: string;
+}
+
+export interface CustomParams {
+  name: string;
+  required?: boolean;
+  suggestions?: string[];
+  template?: string;
+  type: typeof ParamType[keyof typeof ParamType];
 }
