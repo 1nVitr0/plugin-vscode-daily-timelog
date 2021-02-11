e40b259f89330cc2f32194623276b9255ef0713c
1nVitr0 <becker.aram@gmail.com>
2021-02-11 21:59:32 +0100

    feat!: :wastebasket: clean up unused commands
    break and task commands no longer available

--- client/src/contribute/commands.ts
+++ client/src/contribute/commands.ts
@@ -1,11 +1,5 @@
 import { commands, Disposable } from 'vscode';
 import { generateOverview, generateSummary, generateTaskList, startNewDaylog } from '../app/commands/basic';
-import {
-  completeBreakSegment,
-  completeWorkSegment,
-  startBreakSegment,
-  startWorkSegment,
-} from '../app/commands/segments';
 
 export default function contributeCommands(): Disposable[] {
   return [
@@ -13,9 +7,5 @@ export default function contributeCommands(): Disposable[] {
     commands.registerCommand('daily-timelog.generateTaskList', generateTaskList),
     commands.registerCommand('daily-timelog.generateSummary', generateSummary),
     commands.registerCommand('daily-timelog.generateOverview', generateOverview),
-    commands.registerCommand('daily-timelog.startWorkSegment', startWorkSegment),
-    commands.registerCommand('daily-timelog.startBreakSegment', startBreakSegment),
-    commands.registerCommand('daily-timelog.completeWorkSegment', completeWorkSegment),
-    commands.registerCommand('daily-timelog.completeBreakSegment', completeBreakSegment),
   ];
 }
