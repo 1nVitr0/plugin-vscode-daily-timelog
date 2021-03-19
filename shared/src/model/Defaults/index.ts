3f3b523cea585a8bcf040c76ed25a6da675e6a3d
Aram Becker <becker.aram@gmail.com>
2021-03-19 21:30:40 +0100

    feat!: support full template syntax in templates
    BREAKING CHANGE: new templatings syntax(double curly braces)
    BREAKING CHANGE: changed duration template syntax

--- shared/src/model/Defaults/index.ts
+++ shared/src/model/Defaults/index.ts
@@ -2,9 +2,12 @@ import { CST, Document, Options } from 'yaml';
 import { isBreak } from '../../tools/string';
 import { ParamType } from '../Settings/Params';
 import Settings, { BasicSettings } from '../Settings/Settings';
+
 export const defaultBasicSettings: BasicSettings = {
   commonTasks: ['Organization', 'Daily', 'Meeting', 'Refactoring', 'Code Review', 'Checking Mails'],
   commonBreaks: ['Lunch', 'Breakfast', 'Coffee', 'Personal'],
+  taskGroups: ['organisation', 'review', 'evaluation', 'meeting', 'reseach', 'programming'],
+  ticketPrefixes: [],
   customParams: [
     { name: 'goals', type: ParamType.Array, template: '{{nextIndex}}. {{value}}' },
     { name: 'achievements', type: ParamType.Array, template: '{{nextIndex}}. {{value}}' },
@@ -16,7 +19,7 @@ export const defaultBasicSettings: BasicSettings = {
   dateFormat: 'YYYY-MM-DD',
   summaryDateFormat: 'YYYY-MM-DD',
   timeFormat: 'HH:mm',
-  durationFormat: '{{h}}h? {{m}}m?',
+  durationFormat: 'h\\h ?m\\m?',
   durationPrecision: 15,
   durationRounding: 'round',
   minimumDuration: 15,
@@ -34,10 +37,11 @@ export const defaultSettings: Settings = {
   ...defaultBasicSettings,
   autoGenerateOverview: true,
   taskListTitle: "Today's Tasks",
-  taskListDurationFormat: '- {{task.name}}: {{duration}}',
+  taskListDurationFormat: '- {{task.ticket ? "[" + task.ticket + "] " : ""}}{{task.name}}: {{duration}}',
   taskListStructure: ['*{{taskListTitle}} - {{date}}*', '', '{{estimatedDurations}}', '', '= {{estimatedTotals}}'],
   summaryTitle: "Today's Summary",
-  summaryDurationFormat: '- {{task.name}}: {{duration}} {{?:progress}}(?{{progress}})?',
+  summaryDurationFormat:
+    '- {{task.ticket ? "[" + task.ticket + "] " : ""}}{{task.name}}: {{duration}} {{progress ? "(" + progress + ")" : ""}}',
   summaryStructure: ['*{{summaryTitle}} - {{date}}*', '', '{{durations}}', '', '= {{totals}}'],
   newDayTemplate: [
     'date: {{currentDate}}',
@@ -53,7 +57,7 @@ export const defaultSettings: Settings = {
     '  -  "{{workDayHoursStart}}": !begin {{beginDayMessage}}',
   ],
   overviewTitle: '',
-  overviewStructure: ['{{overviewTitle}} -? {{ date }}', '', '{{taskList}}', '{{summary}}'],
+  overviewStructure: ['{{overviewTitle ? overviewTitle + " - " : ""}}{{ date }}', '', '{{taskList}}', '{{summary}}'],
 };
 
 export const yamlCustomTags: Options['customTags'] = [
--- shared/src/tools/time.ts
+++ shared/src/tools/time.ts
@@ -1,6 +1,58 @@
 import moment, { Moment, Duration } from 'moment';
 import { defaultBasicSettings } from '../model/Defaults';
-import { formatString, parseString } from './string';
+import { formatString } from './string';
+
+interface DurationParams {
+  H?: number;
+  h?: number;
+  M?: number;
+  m?: number;
+}
+
+const durationKeys = ['H', 'M', 'h', 'm'];
+
+function parseDurationString(duration: string, format: string) {
+  const parts: { [key in keyof DurationParams]: { content: string } } = {};
+  let escaped = false;
+  let mapped = false;
+  let current = { content: '' };
+  let nextPrefix = '';
+  for (const char of format) {
+    if (char == '\\') escaped = true;
+    else if (durationKeys.indexOf(char) >= 0 && !escaped) {
+      if (mapped) {
+        current = { content: nextPrefix };
+        nextPrefix = '';
+        mapped = false;
+      }
+      current.content += '([\\d.]+)';
+      parts[char as keyof DurationParams] = current;
+      current = parts[char as keyof DurationParams] as { content: string };
+      mapped = true;
+    } else if (char === '?' && !escaped && mapped) {
+      current.content += char;
+      current = { content: '' };
+      nextPrefix = '';
+      mapped = false;
+    } else {
+      current.content += char;
+      if (char !== '\\') escaped = false;
+      if (mapped) nextPrefix += char;
+    }
+  }
+
+  const result: DurationParams = {};
+  for (const key of Object.keys(parts)) {
+    const part = parts[key as keyof DurationParams];
+    if (!part) continue;
+
+    const regex = new RegExp(part.content);
+    const [_, match] = duration.match(regex) || ['', '0'];
+    result[key as keyof DurationParams] = parseFloat(match);
+  }
+
+  return result;
+}
 
 export function validateDate(date: string, settings = defaultBasicSettings): boolean {
   const parsed = moment(date, settings.dateFormat);
@@ -16,13 +68,13 @@ export function formatDate(date: Moment, settings = defaultBasicSettings, summar
 }
 
 export function parseDuration(duration: string, settings = defaultBasicSettings): Duration {
-  const params = parseString(duration, settings.durationFormat);
+  const params = parseDurationString(duration, settings.durationFormat);
   let minutes = 0;
-  if ('M' in params && params.M !== undefined) minutes = parseInt(params.M || '0');
-  else if ('H' in params && params.H !== undefined) minutes = parseFloat(params.H || '0') * 60;
+  if ('M' in params && params.M) minutes = params.M;
+  else if ('H' in params && params.H) minutes = params.H * 60;
   else {
-    if ('h' in params) minutes += (parseInt(params.h || '0') || 0) * 60;
-    if ('m' in params) minutes += parseInt(params.m || '0') || 0;
+    if ('h' in params && params.h) minutes += params.h * 60;
+    if ('m' in params && params.m) minutes += params.m;
   }
 
   return moment.duration(minutes, 'm');
@@ -55,6 +107,27 @@ export function formatDuration(_duration: Duration | number, settings = defaultB
   const h = Math.floor(H);
   const M = Math.floor(duration.asMinutes());
   const m = Math.floor(M) - h * 60;
+  const params: DurationParams = { H, h, M, m };
 
-  return formatString(settings.durationFormat, { H, h, M, m });
+  let result = '';
+  let current = '';
+  let escaped = false;
+  let mapped = false;
+  for (const char of settings.durationFormat) {
+    if (char == '\\') escaped = true;
+    else if (durationKeys.indexOf(char) >= 0 && !escaped) {
+      current += params[char as keyof DurationParams];
+      if (params[char as keyof DurationParams]) mapped = true;
+    } else if (char === '?' && !escaped) {
+      if (mapped) result += current;
+      current = '';
+      mapped = false;
+    } else {
+      current += char;
+      escaped = false;
+    }
+  }
+  if (current) result += current;
+
+  return result;
 }
