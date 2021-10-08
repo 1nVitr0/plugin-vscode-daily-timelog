53bad994b95d16316ee5c0c136eb13a0357fab2a
Aram Becker <becker.aram@gmail.com>
2021-10-08 22:10:35 +0200

    fix: make jira setup more user friendly

--- client/src/app/commands/jira.ts
+++ client/src/app/commands/jira.ts
@@ -1,26 +1,59 @@
-import { env, Uri, window, workspace } from 'vscode';
+import { env, Uri, window, workspace, commands } from 'vscode';
 import { JiraApi } from '../../../../shared/out';
+
+const JIRA_TOKEN_URL = 'https://id.atlassian.com/manage-profile/security/api-tokens';
 
 export async function setupJiraToken() {
   const item = await window.showInformationMessage(
     'Please visit your Atlassian profile page and set up an API Token. Copy it and return here.',
-    'Open profile page'
+    'Open profile page',
+    'I have an API token'
   );
-  if (item) env.openExternal(Uri.parse('https://id.atlassian.com/manage-profile/security/api-tokens'));
+  if (item == 'Open profile page') {
+    env.openExternal(Uri.parse(JIRA_TOKEN_URL));
+    const answer = await window.showInformationMessage(
+      'After creating anc copying your API Token, click `Continue`',
+      'Continue',
+      'Restart'
+    );
+    if (answer !== 'Continue') {
+      if (answer) commands.executeCommand('daily-timelog.setupJiraToken');
+      return {};
+    }
+  }
   const token = await window.showInputBox({
     title: 'Enter your atlassian API token:',
     placeHolder: 'TOKEN',
     password: true,
+    ignoreFocusOut: true,
+    validateInput: (input) => (!input ? 'Please enter a valid token' : undefined),
   });
-  const domain = await window.showInputBox({ title: 'Enter your atlassian domain:', placeHolder: 'DOMAIN' });
-  const email = await window.showInputBox({ title: 'Enter your atlassian email:', placeHolder: `user@${domain}.com` });
+  const domain = await window.showInputBox({
+    title: 'Enter your atlassian domain:',
+    placeHolder: 'DOMAIN',
+    ignoreFocusOut: true,
+    validateInput: (input) => (!input ? 'Please enter a valid domain' : undefined),
+  });
+  const email = await window.showInputBox({
+    title: 'Enter your atlassian email:',
+    placeHolder: `user@${domain}.com`,
+    ignoreFocusOut: true,
+    validateInput: (input) => (!input ? 'Please enter a valid email' : undefined),
+  });
 
-  if (!token) return null;
+  if (!token || !domain || !email) {
+    window.showErrorMessage('Invalid credentials');
+    return {};
+  }
 
   const configuration = workspace.getConfiguration('daily-timelog');
-  await configuration.update('jiraToken', token);
-  await configuration.update('jiraDomain', domain);
-  await configuration.update('jiraUserEmail', email);
+  try {
+    await configuration.update('jiraToken', token);
+    await configuration.update('jiraDomain', domain);
+    await configuration.update('jiraUserEmail', email);
+  } catch (e) {
+    console.error(e);
+  }
 
   return { token, domain, user: email };
 }
@@ -40,7 +73,14 @@ export async function setupJiraUser(domain?: string, email?: string, token?: str
   const api = new JiraApi(domain, email, token);
   const users = await api.getUsers();
 
-  const selectedUser = await window.showQuickPick(users.map(({ displayName }) => displayName));
+  const selectedUser = await window.showQuickPick(
+    users.map(({ displayName }) => displayName),
+    {
+      title: 'Select current Jira user',
+      placeHolder: 'USER',
+      ignoreFocusOut: true,
+    }
+  );
   const accountId = users.find(({ displayName }) => displayName == selectedUser)?.accountId;
 
   if (!accountId) return null;
@@ -52,5 +92,6 @@ export async function setupJiraUser(domain?: string, email?: string, token?: str
 
 export async function setupJira() {
   const { domain, user, token } = await setupJiraToken();
+  if (!domain || !user || !token) return;
   await setupJiraUser(domain, user, token);
 }
